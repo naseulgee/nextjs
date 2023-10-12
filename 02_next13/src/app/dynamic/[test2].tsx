6849710230ee8c5195581c834ced838a0ac13d11
seulgee <nseulgee@gmail.com>
2023-10-12 10:32:55 +0900

    레이아웃과 그룹핑용 폴더, 동적경로

--- 02_next13/src/app/dynamic/[test2].tsx
+++ 02_next13/src/app/dynamic/[test2].tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>HomePage</h1>
+      [test2].tsx
     </main>
   )
 }
--- 02_next13/src/app/layout.tsx
+++ 02_next13/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +15,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const aa = "/aa";
+  const bb = "/bb";
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        루트 레이아웃<br/>
+        <nav>
+          <Link href="/">HOME</Link><br/>
+          <Link href="/blog">Blog</Link><br/>
+          <Link href="/about">About</Link><br/>
+          <Link href={"/dynamic/" + aa}>DynamicA</Link><br/>
+          <Link href={"/dynamic/" + bb}>DynamicB</Link><br/>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
